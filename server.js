9d7995c45b25ddeb15d6a63a19b703c3c61d454d
SalmanDanesh93 <salmand64@Yahoo.com>
2022-09-26 07:27:52 -0700

    edits

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Fonzidanesh21',
+    password: '',
     database: 'employee_db'
 });
 
